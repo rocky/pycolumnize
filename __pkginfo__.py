7cf8db1f09bd07acb2d44081858dcde8a86ba24c
rocky.bernstein <rocky.bernstein@220420d0-c99f-11dd-87b6-9d1a99559f42>
2010-10-28 12:02:19 +0000

    Get ready for release
    git-svn-id: https://pycolumnize.googlecode.com/svn/trunk@32 220420d0-c99f-11dd-87b6-9d1a99559f42

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -1,27 +1,37 @@
 """packaging information"""
-
-modname = 'columnize'
-
-numversion = (0, 3, 3)
-version = '.'.join([str(num) for num in numversion])
-
-short_desc = 'Format a simple (i.e. not nested) list into aligned columns.'
-
-author = "Rocky Bernstein"
-author_email = "rocky@gnu.org"
-
-# web = 'http://code.google.com/p/pycolumnize'
-# ftp = "ftp://ftp.gnu.org/pub/gnu/libcdio/%s-%s.tar.gz" % (modname, version)
-# mailinglist = "mailto:libcdio-pycdio-devel@gnu.org"
-
+# Things that change more often go here.
+copyright   = '''Copyright (C) 2008, 2009, 2010 Rocky Bernstein <rocky@gnu.org>.'''
 classifiers =  ['Development Status :: 4 - Beta',
                 'Intended Audience :: Developers',
                 'License :: OSI Approved :: Python Software Foundation License',
                 'Programming Language :: Python',
                 'Topic :: Software Development :: Libraries :: Python Modules',
                 ]
-# download_url = '%s-%s.egg' % (modname, version,)
 
+# The rest in alphabetic order
+author       = "Rocky Bernstein"
+author_email = "rocky@gnu.org"
+ftp_url      = None
+license      = 'PSF2'
+mailing_list = None
+modname      = 'columnize'
+
+short_desc = 'Format a simple (i.e. not nested) list into aligned columns.'
 py_modules = [modname]
 
+# VERSION.py sets variable VERSION.
+import os.path
+execfile(os.path.join(os.path.dirname(__file__), 'VERSION.py'))
 web = 'http://code.google.com/p/pycolumnize'
+
+# download_url = '%s-%s.egg' % (modname, version,)
+
+
+web = 'http://code.google.com/p/pycolumnize'
+# tracebacks in zip files are funky and not debuggable
+zip_safe = False 
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+long_description   = ( read("README.txt") + '\n\n' +  read("NEWS") )
+
--- setup.py
+++ setup.py
@@ -5,29 +5,26 @@ distutils setup (setup.py)
 This gets a bit of package info from __pkginfo__.py file
 """
 # Get the required package information 
-from __pkginfo__ import author, author_email, classifiers, \
-    py_modules, modname, \
-    short_desc, version, web
+from __pkginfo__ import \
+    author,           author_email,       classifiers,      ftp_url,      \
+    license,          long_description,   mailing_list,                   \
+    modname,          py_modules,                                         \
+    short_desc,       VERSION, web, zip_safe
 
+__import__('pkg_resources')
 from setuptools import setup
-
-import os
-top_dir = os.path.dirname(__file__)
-README  = os.path.join(top_dir, 'README.txt')
-
-# Description in package will come from the README file.
-long_description = open(README).read() + '\n\n'
 
 setup(
       author             = author,
       author_email       = author_email,
       classifiers        = classifiers,
       description        = short_desc,
-      # license            = license,
+      license            = license,
+      py_modules         = py_modules,
       long_description   = long_description,
       name               = modname,
-      py_modules         = py_modules,
       test_suite         = 'nose.collector',
       url                = web,
-      version            = version,
+      version            = VERSION,
+      zip_safe           = zip_safe
       )
