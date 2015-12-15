fd4325df0ad08424a4a547ab6f7e6208e8346cd8
rocky <rocky@gnu.org>
2015-12-15 02:16:24 -0500

    Note for ActiveState that we support lots of Python versoins

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -8,6 +8,16 @@ classifiers =  ['Development Status :: 5 - Production/Stable',
                 'License :: OSI Approved :: Python Software Foundation License',
                 'Programming Language :: Python',
                 'Topic :: Software Development :: Libraries :: Python Modules',
+                'Programming Language :: Python :: 2.4',
+                'Programming Language :: Python :: 2.5',
+                'Programming Language :: Python :: 2.6',
+                'Programming Language :: Python :: 2.7',
+                'Programming Language :: Python :: 3.0',
+                'Programming Language :: Python :: 3.1',
+                'Programming Language :: Python :: 3.2',
+                'Programming Language :: Python :: 3.3',
+                'Programming Language :: Python :: 3.4',
+                'Programming Language :: Python :: 3.5 ',
                 ]
 
 # The rest in alphabetic order
